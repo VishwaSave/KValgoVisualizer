61eb4da03ee34ada958d0c10ec03fb232a6535e7
Vishwa <vishwasave41@gmail.com>
2024-03-14 01:10:35 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
+import Sorting from "./Component/SortingAlgo/Sorting";
+import LinkedListVisualizer from "./Component/LinkedList/LinkedList";
+import BST from "./Component/BST/bst";
 function App() {
   return (
-    <div>hii</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sorting-algorithms" element={<Sorting />} />
+        <Route path="/linkedlist-algorithms" element={<LinkedListVisualizer />} />
+        <Route path="/bst-algorithms" element={<BST />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
