d6e0539fa258299ba44f695116f8d23de5a7f659
Vishwa <vishwasave41@gmail.com>
2024-03-14 01:01:41 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Component/Home";
-import Sorting from "./Component/SortingAlgo/Sorting";
-import LinkedListVisualizer from "./Component/LinkedList/LinkedList";
-import BST from "./Component/BST/bst";
 function App() {
   return (
     <div>hii</div>
